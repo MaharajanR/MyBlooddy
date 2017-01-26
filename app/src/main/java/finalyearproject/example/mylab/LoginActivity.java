ae344dbe3f2f3537cbca0ab800b91d25dd48ba83
Maharajan CSE <Maharajan CSE>
2017-01-26 22:37:35 +0530

    Basic App Init

---
+++ app/src/main/java/finalyearproject/example/mylab/LoginActivity.java
@@ -0,0 +1,38 @@
+package finalyearproject.example.mylab;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+
+import static android.R.attr.button;
+
+public class LoginActivity extends AppCompatActivity{
+    private Button login,register;
+    private EditText etUser,etPass;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_login);
+        etPass= (EditText)findViewById(R.id.etPass);
+        etUser= (EditText)findViewById(R.id.etUser);
+        login = (Button)findViewById(R.id.btnLogin);
+        register = (Button)findViewById(R.id.btnReg);
+        login.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+
+            }
+        });
+        register.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+
+            }
+        });
+    }
+
+
+}
+

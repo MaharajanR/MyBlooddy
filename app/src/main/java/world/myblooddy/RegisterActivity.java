39c4e406c81a257a3b54d7804682ca0da08098c6
MaharajanR <maharajanr6@gmail.com>
2017-01-27 12:02:42 +0530

    Register Activity Changed

--- app/src/main/java/world/myblooddy/RegisterActivity.java
+++ app/src/main/java/world/myblooddy/RegisterActivity.java
@@ -7,7 +7,7 @@ import android.widget.Button;
 import android.widget.EditText;
 import android.widget.TextView;
 
-public class RegisterActivity extends AppCompatActivity implements View.OnClickListener {
+public class RegisterActivity extends AppCompatActivity  {
     private Button reg;
     private TextView tvLogin;
     private EditText etUser,etPass;
@@ -16,24 +16,13 @@ public class RegisterActivity extends AppCompatActivity implements View.OnClickL
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_register);
-        tvLogin=(TextView)findViewById(R.id.tvLogin);
+       // tvLogin=(TextView)findViewById(R.id.tvLogin);
         etUser= (EditText) findViewById(R.id.etUser);
         reg = (Button) findViewById(R.id.btnReg);
-        reg.setOnClickListener(this);
-        tvLogin.setOnClickListener(this);
+//        reg.setOnClickListener(this);
+//        tvLogin.setOnClickListener(this);
 
     }
 
-    @Override
-    public void onClick(View v) {
-        switch (v.getId()) {
-            case R.id.btnReg:
 
-                break;
-            case R.id.tvLogin:
-                break;
-
-            default:
-        }
     }
-}
